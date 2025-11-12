28711a7815a77be94059c5a17f142dd0c0d72833
juiyzzone97-스택 <juoyzzone97@gmail.com>
2025-11-12 14:05:29 +0900

    fs모듈을 통한 로그인 기능 구현

--- models/User.js
+++ models/User.js
@@ -7,16 +7,22 @@ class User {
     this.body = body;
   }
 
-  login() {
-    const body = this.body;
-    const {id,password} = UserStorage.getUsersInfo(body.id);
+  async login() {
+    const client = this.body;
+    const {id, password} = await UserStorage.getUsersInfo(client.id);
     if(id) {
-      if(id === body.id && password === body.password) {
+      if(id === client.id && password === client.password) {
         return {success : true};
       }
       return { success : false, message : "비밀번호가 틀립니다."};
     }
     return {success: false, message : "존재하지 않는 아이디입니다."}
+  }
+
+  register(){
+    const client = this.body;
+    const response = UserStorage.save(client)
+    return response;
   }
 }
 
--- models/UserStorage.js
+++ models/UserStorage.js
@@ -1,13 +1,20 @@
 
+const fs = require("fs").promises;
 
 class UserStorage {
-  static #users = {
-  id : ["오하얀", "김현아"],
-  password : ["1234", "5678"],
-  name : ["오하얀1", "김현아1"]
-}
+  static #getUserInfo(data, id) {
+    const users = JSON.parse(data)
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users)
+        const userInfo = usersKeys.reduce((newUser, info) =>{
+        newUser[info] = users[info][idx];
+        return newUser;
+        }, {});
+        return userInfo
+  }
+  
   static getUsers(...fields) {
-    const users = this.#users;
+    // const users = this.#users;
     const newUsers = fields.reduce((newUsers, field) =>{
       if(users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
@@ -18,15 +25,21 @@ class UserStorage {
   }
 
   static getUsersInfo(id) {
-    const users = this.#users;
-    const idx = users.id.indexOf(id);
-    const usersKeys = Object.keys(users)
-    const userInfo = usersKeys.reduce((newUser, info) =>{
-      newUser[info] = users[info][idx];
-      return newUser;
-    }, {});
+    return fs
+      .readFile("./databases/users.json")
+      .then((data) => {
+          return this.#getUserInfo(data, id);
+      })
+      .catch(console.error);
+    }
+  
 
-    return userInfo;
+  static save(userInfo) {
+    // const users = this.#users;
+    users.id.push(userInfo.id);
+    users.name.push(userInfo.name);
+    users.password.push(userInfo.password);
+    return { success: true};
   }
 }
 
